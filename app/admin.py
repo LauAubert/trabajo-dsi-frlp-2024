0910ef58ddd1cf40fff54de2fab6d40ffdcdbbe3
LauAubert <aubertlautaroa@gmail.com>
2024-12-02 23:49:19 -0300

    añadida db y mejorado el panel de admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import *
+
+class ClasificacionInline(admin.TabularInline):
+    model = Clasificacion
+    extra = 1
+
+class PruebaAdmin(admin.ModelAdmin):
+    inlines = [ClasificacionInline]
+
 # Register your models here.
 admin.site.register(Pais)
 admin.site.register(Ciclista)
 admin.site.register(Director)
 admin.site.register(Equipo)
-admin.site.register(Prueba)
+admin.site.register(Prueba, PruebaAdmin)
 admin.site.register(Clasificacion)
